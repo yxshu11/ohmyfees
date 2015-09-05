3af3da6bbe8378eee35c48c66ca983cb6dc34833
Mac Shu <mac_shu11@hotmail.com>
2015-09-05 23:48:25 +0800

    Major Facelift for interface, added in Dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root                'landing_pages#home'
   get 'help'      =>  'landing_pages#help'
   get 'contact'   =>  'landing_pages#contact'
+  get 'dashboard' =>  'landing_pages#dashboard'
 
 end
