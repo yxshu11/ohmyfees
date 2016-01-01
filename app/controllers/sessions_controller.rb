c6271a633611793e853e375722b2a948c5433ccb
Mac Shu <mac_shu11@hotmail.com>
2016-01-02 00:37:58 +0800

    Added current_user and logged_in method for session helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,12 @@ class SessionsController < ApplicationController
     # Get the student obj by searching the email
     if params[:type] == 'Student'
       # Fetch user from the Student Model
-      user = Student.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
+      student = Student.find_by(email: params[:session][:email].downcase)
+      if student && student.authenticate(params[:session][:password])
         # Log In and Handle something
-        log_in user
-        redirect_to user
+        flash[:success] = "Log In Successfully!"
+        log_in student,params[:type]
+        redirect_to student
       else
         # Create an Error Message
         flash.now[:danger] = 'Invalid email/password combination.'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,24 @@
 module SessionsHelper
 
   # Logs in the given users
-  def log_in(student)
-    session[:student_id] = student.id
+  def log_in(user, type)
+    session[:type] = type
+    session[:user_id] = user.id
   end
 
+  # Returns the current logged-in user (if any)
   def current_user
-    Student.find_by(id: session[:student_id])
+    if sessions[:type] == "Student"
+      @current_user ||= Student.find_by(id: session[:user_id])
+    elsif session[:type] == "Staff"
+      @current_user ||= Staff.find_by(id: session[:user_id])
+    end
   end
 
+  # Returns true if the user is logged in, false otherwise
+  def logged_in?
+      !current_user.nil?
+  end
   
+
 end
