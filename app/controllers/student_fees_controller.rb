519f90aa5c82bd55636bd83e9568859f73cf3e8d
Mac Shu <mac_shu11@hotmail.com>
2016-01-12 16:23:46 +0800

    Refined some mistake in student fees controller and views

--- app/controllers/student_fees_controller.rb
+++ app/controllers/student_fees_controller.rb
@@ -13,7 +13,7 @@ class StudentFeesController < ApplicationController
   def show
     if current_user_type == "Student"
       # Display the fees that belong only the to the signed in student
-      @student_fees = current_user.student_fees.find(params[:id])
+      @student_fee = current_user.student_fees.find(params[:id])
     elsif current_user_type == "Staff"
       # Display the all the fees in the system for the signed in staff.
       @student_fee = StudentFee.find(params[:id])
