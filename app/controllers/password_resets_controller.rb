ca607567d9c4475dfe4600a122bc702d97fdfa16
Mac Shu <mac_shu11@hotmail.com>
2016-01-14 12:02:28 +0800

    Refine password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -61,6 +61,7 @@ class PasswordResetsController < ApplicationController
       if @student.password_reset_expired?
         flash[:danger] = "Password reset has expired."
         redirect_to new_password_reset_url
+      end
     end
 
 end
