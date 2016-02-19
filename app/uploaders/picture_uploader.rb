aa600e7d3734c4cb6039c02b5d044efdd407e379
Mac Shu <mac_shu11@hotmail.com>
2016-02-19 16:07:12 +0800

    Refined PictureUploader and gitignore file

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,9 +16,4 @@ class PictureUploader < CarrierWave::Uploader::Base
     eager
     resize_to_fit(100, 100)
   end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
 end
