2310b34b012cc9faff1acf5b5f4f30bb7b0e2952
Mac Shu <mac_shu11@hotmail.com>
2016-02-29 13:39:21 +0800

    Added Report Views and Controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,13 @@
 class ReportsController < ApplicationController
+
   def show
+    @payments = Payment.paginate(page: params[:page])
+
+    @total = 0
+
+    @total_payments_received = Payment.count
+    @payments.each do |payment|
+      @total = @total + payment.amount.to_i
+    end
   end
 end
