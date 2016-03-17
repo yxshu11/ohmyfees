a9e6af90f890fca7cae5d4becd56ae8bc77a93af
Mac Shu <mac_shu11@hotmail.com>
2016-03-17 22:37:42 +0800

    Added Rack Deflater for content compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,9 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enabling browser side caching
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=#{1.month.to_i}"
-  
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
